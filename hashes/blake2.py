11ca1dae8ee9afefd03e3fddd38dbea315fe2940
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2023-07-24 23:55:07 -0600

    Ejemplo Blake

--- hashes/blake2.py
+++ hashes/blake2.py
@@ -4,3 +4,11 @@
 
 # Sin embargo, es altamente paralelizable, por lo que es conveniente
 # cuando hay que procesar muchos datos
+
+from hashlib import blake2b
+
+b = blake2b()
+b.update(b"1234567890123456")
+
+print("Blake 2b", b.hexdigest())
+print("Longitud", len(b.digest()))
